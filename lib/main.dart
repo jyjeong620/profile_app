9db89d00975bfb3747ee965f69778a3fcd20dfa4
정준영 <jeongjy@mrblue.com>
2022-04-30 10:21:53 +0900

    feat: 초기틀 구성

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,8 @@
-void main() {}
+import 'package:flutter/cupertino.dart';
+import 'package:profile_app/profile_page.dart';
+
+
+
+void main() {
+  runApp(const ProfilePage());
+}
---
+++ lib/profile_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Container(
+          color: Colors.black,
+          child: Stack(
+            children: [
+              FractionallySizedBox(),
+              FractionallySizedBox(),
+              ClipOval()
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
