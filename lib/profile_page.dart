45a9218c6f806120740ab171ce70536b0fde6fbd
정준영 <jeongjy@mrblue.com>
2022-05-01 18:09:34 +0900

    refactor: 기능별 메소드화

--- lib/profile_page.dart
+++ lib/profile_page.dart
@@ -13,90 +13,106 @@ class ProfilePage extends StatelessWidget {
           color: Colors.black,
           child: Stack(
             children: [
-              FractionallySizedBox(
-                heightFactor: 0.5,
-                child: Image.asset(
-                  "assets/images/background.jpeg",
-                  // fit: BoxFit.cover,
-                ),
-              ),
-              Align(
-                alignment: Alignment.bottomCenter,
-                child: FractionallySizedBox(
-                  heightFactor: 0.35,
-                  child: Column(
-                    children: [
-                      const Text(
-                        "Hi! I'm Edgar!",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 50,
-                          fontFamily: "cursive",
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                      const SizedBox(height: 15),
-                      const Text(
-                        "I'm a programmer!\nI like Holiday",
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 24,
-                            fontFamily: "sans-serif-medium",
-                            fontWeight: FontWeight.w300),
-                        textAlign: TextAlign.center,
-                      ),
-                      const SizedBox(height: 20),
-                      const Divider(
-                        height: 1.5,
-                        color: Colors.white,
-                        indent: 40,
-                        endIndent: 40,
-                      ),
-                      const SizedBox(height: 20),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          ClipOval(
-                            child: SizedBox(
-                              width: 80,
-                              height: 80,
-                              child: MaterialButton(
-                                onPressed: () {},
-                                child: const Icon(Icons.email),
-                              ),
-                            ),
-                          ),
-                          ClipOval(
-                            child: SizedBox(
-                              width: 80,
-                              height: 80,
-                              child: MaterialButton(
-                                onPressed: () {},
-                                child: const Icon(Icons.email),
-                              ),
-                            ),
-                          ),
-                          ClipOval(
-                            child: SizedBox(
-                              width: 80,
-                              height: 80,
-                              child: MaterialButton(
-                                onPressed: () {},
-                                child: const Icon(Icons.email),
-                              ),
-                            ),
-                          )
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-              ),
+              _createBackground(),
+              _createContents(),
               const ClipOval()
             ],
           ),
         ),
       ),
+    );
+  }
+  Widget _createBackground() {
+    return FractionallySizedBox(
+      heightFactor: 0.5,
+      child: Image.asset(
+        "assets/images/background.jpeg",
+        // fit: BoxFit.cover,
+      ),
+    );
+  }
+
+  Widget _createContents() {
+    return Align(
+      alignment: Alignment.bottomCenter,
+      child: FractionallySizedBox(
+        heightFactor: 0.37,
+        child: Column(
+          children: [
+            _createIntroduce(),
+            const SizedBox(height: 15),
+            _createMessage(),
+            const SizedBox(height: 20),
+            const Divider(
+              height: 1.5,
+              color: Colors.white,
+              indent: 40,
+              endIndent: 40,
+            ),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ClipOval(
+                  child: SizedBox(
+                    width: 70,
+                    height: 70,
+                    child: MaterialButton(
+                      onPressed: () {},
+                      child: Image.asset("assets/images/blog.png")
+                    ),
+                  ),
+                ),
+                ClipOval(
+                  child: SizedBox(
+                    width: 70,
+                    height: 70,
+                    child: MaterialButton(
+                      onPressed: () {},
+                        child: Image.asset("assets/images/github.png")
+                    ),
+                  ),
+                ),
+                ClipOval(
+                  child: SizedBox(
+                    width: 70,
+                    height: 70,
+                    child: MaterialButton(
+                        onPressed: () {},
+                        child: Image.asset("assets/images/email.png")
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _createIntroduce() {
+    return const Text(
+      "Hi! I'm Edgar!",
+      style: TextStyle(
+        color: Colors.white,
+        fontSize: 50,
+        fontFamily: "cursive",
+        fontWeight: FontWeight.w600,
+      ),
+    );
+  }
+
+  Widget _createMessage() {
+    return const Text(
+      "I'm a programmer!\nI like Holiday",
+      style: TextStyle(
+          color: Colors.white,
+          fontSize: 24,
+          fontFamily: "sans-serif-medium",
+          fontWeight: FontWeight.w300),
+      textAlign: TextAlign.center,
     );
   }
 }
